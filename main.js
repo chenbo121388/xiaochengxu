83ca58e3398e74e12053312f1a7f2d5111e05a70
chenbo <1397929163@qq.com>
2022-05-09 15:45:28 +0800

    完成home页

--- main.js
+++ main.js
@@ -3,6 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 
+
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+$http.beforeRequest = function (options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+uni.$showMsg = function (title = '数据加载失败',duration = 1500,icon= 'none'){
+	uni.showToast({
+		title,
+		duration,
+		icon
+	})
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		1233
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
