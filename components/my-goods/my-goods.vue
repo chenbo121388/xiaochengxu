24f26c396e6a38df1c194258c42501b70bb810d2
chenbo <1397929163@qq.com>
2022-05-12 14:47:35 +0800

    完成goodslist

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+	<view>
+		<view class="good-item">
+			<view class="good-item-left">
+				<image :src="good.goods_small_logo" class="goods-pic"></image>
+			</view>
+			<view class="good-item-right">
+				<view class="good-name">{{good.goods_name}}</view>
+				<view class="good-info-box">
+					<view class="goods-price">￥{{good.goods_price | tofixed}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			good:{
+				type:Object,
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		filters:{
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.good-item{
+	display: flex;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.good-item-left{
+		margin-right: 5px;
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.good-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.good-name{
+			font-size: 13px;
+		}
+		.goods-price{
+			font-size: 16px;
+			color: #c00000;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		133
+		<view class="goods_list">
+			<view v-for="(good,i) in goodsList" :key="i" @click="gotoDetail(good)">
+				<my-goods :good="good"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,52 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading:false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || '',
+				this.queryObj.cid = options.cid || '',
+				this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.total = res.message.total
+				this.isloading = false
+				cb && cb()
+			},
+			gotoDetail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			}
+		},
+		onReachBottom(){
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			if(this.isloading) return
+			this.queryObj.pagenum += 1
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
 		}
 	}
 </script>
